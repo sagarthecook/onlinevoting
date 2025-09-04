436daa94dbe725233ef982339867b2694fe5dc85
thecook060 <thecook060@gmail.com>
2025-09-04 20:09:13 +0530

    origin code added

--- src/main/java/com/onlinevoting/controller/UserLoginController.java
+++ src/main/java/com/onlinevoting/controller/UserLoginController.java
@@ -2,6 +2,7 @@ package com.onlinevoting.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import com.onlinevoting.service.LoginService;
 import jakarta.validation.Valid;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UserLoginController {
 
     @Autowired
