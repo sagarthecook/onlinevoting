7607957891d2b0d14a9c104602a81d1d629efbe3
thecook060 <thecook060@gmail.com>
2025-10-02 10:55:43 +0530

    Approved and Rejecged email with status patch mapping added

--- src/main/java/com/onlinevoting/repository/UserDetailRepository.java
+++ src/main/java/com/onlinevoting/repository/UserDetailRepository.java
@@ -2,6 +2,7 @@ package com.onlinevoting.repository;
 
 import com.onlinevoting.model.UserDetail;
 
+
 import java.util.List;
 import java.util.Optional;
 
@@ -10,9 +11,16 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface UserDetailRepository extends JpaRepository<UserDetail, Long> {
     UserDetail findByEmailId(String emailId);
+
+    Optional<UserDetail> findById(Long id);
+   
     Optional<UserDetail> findByIdAndIsActiveTrue(Long id);
+
     List<UserDetail> findByIsActiveFalse();
-    
-    @Query("SELECT u FROM UserDetail u WHERE u.isActive = :isActive AND u.status = :status")
-    List<UserDetail> findByIsActiveAndStatus(Boolean isActive, String status);
+
+    @Query("SELECT userDetail.firstName, userDetail.lastName, userDetail.emailId, userDetail.id, " +
+    "userDetail.status FROM UserDetail userDetail WHERE userDetail.isActive = :isActive AND userDetail.status = :status")
+    List<Object[]> findByIsActiveAndStatus(Boolean isActive, String status);
 }
+
+
