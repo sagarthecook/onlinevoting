fb426640b476051f693469c1cc01c0af719a3606
thecook060 <thecook060@gmail.com>
2025-10-21 10:23:48 +0530

    removed unused import

--- src/main/java/com/onlinevoting/service/MenuServiceImpl.java
+++ src/main/java/com/onlinevoting/service/MenuServiceImpl.java
@@ -7,7 +7,6 @@ import java.util.Map;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.onlinevoting.dto.MenuDto;
