e138d638c785033452f98692a98e1fba06fda726
thecook060 <thecook060@gmail.com>
2025-12-07 18:39:29 +0530

    Origin added

--- src/main/java/com/onlinevoting/controller/UserDetailsController.java
+++ src/main/java/com/onlinevoting/controller/UserDetailsController.java
@@ -16,6 +16,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -28,6 +29,7 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UserDetailsController {
 
     @Autowired
